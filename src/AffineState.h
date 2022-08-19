ed421cf645a2ccf591ed9ab38644758667bd20d2
Vlad Gheorghiu <vsoftco@gmail.com>
2022-08-19 11:45:21 -0400

    switch to CMake

---
+++ src/AffineState.h
@@ -0,0 +1,69 @@
+#ifndef AFFINE_STATE_H_
+#define AFFINE_STATE_H_
+
+#include <Eigen/Dense>
+#include <iostream>
+#include <vector>
+#include <map>
+#include <random>
+
+// using namespace Eigen;
+
+class AffineState {
+  public:
+    // State is represented by exp(i*pi*phase/8)/sqrt(2^r) \sum_{x \in
+    // \{0,1\}^r} i^{x^T Q x} \ket{Ax + b mod 2}, where A is n\times r and has
+    // rank r <= n.
+    int n;     // number of qubits
+    int phase; // global phase is exp(i*pi*phase/8)
+    Eigen::Matrix<int, Eigen::Dynamic, Eigen::Dynamic> Q; // quadratic function
+    Eigen::Matrix<int, Eigen::Dynamic, Eigen::Dynamic>
+        A;                                // affine space generating matrix
+    Eigen::Vector<int, Eigen::Dynamic> b; // affine space offset
+    std::map<int, int>
+        pivots; // AKA "principal index map." Keys are columns, values are the
+                // rows that contain pivots in those columns. Note that we are
+                // using zero-indexing, so the smallest key (assuming the map is
+                // nonempty) will always be 0, and the corresponding value is
+                // the index of the row that has a pivot in column 0.
+
+    AffineState(int m);
+
+    // Gates and measurements
+    void CZ(int a, int b);
+    void CX(int a, int b);
+    void S(int j);
+    void H(int a);
+    void X(int j);
+    void Y(int j);
+    void Z(int j);
+    int MeasureZ(int j); // Returns outcome and updates state
+
+    // Option to print state
+    friend std::ostream& operator<<(std::ostream& out, AffineState const& psi);
+
+  private:
+    int r; // Technically unnecessary since this is just A.cols(), but it is
+           // handy to not have to declare it each time
+
+    // Subroutines
+    void FixFinalBit(int z);
+    void ReduceGramRowCol(int c);
+    void ReindexSubtColumn(int k, int c);
+    void ReindexSwapColumns(int k, int c);
+    void MakePrincipal(int c, int j);
+    void ReselectPrincipalRow(int j, int c);
+    void ZeroColumnElim(int c);
+
+    void
+    ReduceQ(); // Reduces mod 4 on the diagonal and mod 2 on the off-diagonal
+
+    void print(); // Only used for printing state at intermediate stages of the
+                  // calculation when diagnosing errors
+};
+
+// Small helper functions. Not sure if this is the best place to declare them?
+void ReduceMatrixMod(Eigen::MatrixXi& M, int modulus);
+void ReduceVectorMod(Eigen::VectorXi& v, int modulus);
+
+#endif // AFFINE_STATE_H_
