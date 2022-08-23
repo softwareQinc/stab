15d3d3657c42e5415ae5d6422e676ac64b31fe70
Scott Xu <525125scott@gmail.com>
2022-08-23 14:11:27 -0400

    qasmtools visitor template

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 
 #include "AffineState.h"
 #include "random.h"
+#include "qasm/qasm.hpp"
 
 int main() {
     using namespace stab;
@@ -41,4 +42,8 @@ int main() {
         std::cout << random_real(-10.0, 10.0) << ' ';*/
 
     std::cout << std::endl;
+
+    /*std::string prog("OPENQASM 2.0;\ninclude \"qelib1.inc\";\nqreg q[2];\ncreg c[2];\nh q[0];\ncx q[0], q[1];\nmeasure q[0] -> c[0];\nmeasure q[1] -> c[1];\n");
+    std::istringstream prog_stream(prog);
+    qasm::simulate(prog_stream);*/
 }
