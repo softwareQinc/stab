bbe2566dc03f12d093ae36cb2a099b8b3f2055a3
Vlad Gheorghiu <vsoftco@gmail.com>
2022-10-04 08:55:52 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
+#include <memory>
 #include <sstream>
 #include <fstream>
 #include <string>
 #include <random>
 #include <chrono>
 #include <vector>
+#include <memory>
 
 #include <Eigen/Dense>
 #include <qpp/qpp.h>
@@ -15,8 +17,23 @@
 #include "qasm/qasm.hpp"
 
 int main() {
-    Eigen::MatrixXd data(4, 4);
-    data << 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16;
-    Eigen::Ref<Eigen::MatrixXd> ref{data};
-    std::cout << ref << '\n';
+    Eigen::MatrixXi m(4, 4);
+    m << 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16;
+    std::cout << m << "\n\n";
+
+    using block_t = Eigen::Block<Eigen::MatrixXi>; // this is our type
+    // this will be our smart pointer to matrix that'll be shared by all member functions;
+    // takes care of memory allocations/de-allocations automatically with zero overhead
+    std::unique_ptr<block_t> Q;
+
+    Q = std::make_unique<block_t>(m, 0, 0, 2, 2);
+    std::cout << Q->rows() << "x" << Q->cols() << "\n";
+    std::cout << *Q << "\n\n"; // use *Q to dereference the pointer and refer to the matrix
+    (*Q)(0, 0) = 100;
+
+    Q = std::make_unique<block_t>(m, 0, 0, 3, 3);
+    std::cout << Q->rows() << "x" << Q->cols() << "\n";
+    std::cout << *Q << "\n\n";
+
+    std::cout << (*Q) * (*Q) << std::endl;
 }
