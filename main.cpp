c6830168b42599bb629d7238970516b61f64bd23
Vlad Gheorghiu <vsoftco@gmail.com>
2022-10-03 20:36:00 -0400

    fixed CMakeLists

--- main.cpp
+++ main.cpp
@@ -15,5 +15,8 @@
 #include "qasm/qasm.hpp"
 
 int main() {
-
+    Eigen::MatrixXd data(4, 4);
+    data << 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16;
+    Eigen::Ref<Eigen::MatrixXd> ref{data};
+    std::cout << ref << '\n';
 }
