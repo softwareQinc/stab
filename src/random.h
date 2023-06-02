9396ad53ad1785dbdcc433aa238be2756f8c9357
Vlad Gheorghiu <vsoftco@gmail.com>
2023-06-02 14:56:06 -0400

    Update random.h

--- src/random.h
+++ src/random.h
@@ -10,27 +10,22 @@ int random_bit(double p = 0.5);
 
 // template functions need to be defined in the header
 
-template <class T = int>
+template <class T = int, std::enable_if_t<std::is_integral_v<T>>* = nullptr>
 T random_integer(T a = std::numeric_limits<T>::min(),
                  T b = std::numeric_limits<T>::max()) {
     std::uniform_int_distribution<T> dist(a, b);
     if (a > b) {
         throw std::out_of_range("a > b");
     }
-    if constexpr (!std::is_integral_v<T>) {
-        throw std::runtime_error("Type must be integral");
-    }
     return dist(get_prng_engine());
 }
 
-template <class T = double>
+template <class T = double,
+          std::enable_if_t<std::is_floating_point_v<T>>* = nullptr>
 T random_real(T a = std::numeric_limits<T>::min(),
               T b = std::numeric_limits<T>::max()) {
     if (a >= b) {
         throw std::out_of_range("a >= b");
-    }
-    if constexpr (!std::is_floating_point_v<T>) {
-        throw std::runtime_error("Type must be floating point");
     }
     std::uniform_real_distribution<T> dist(a, b);
     return dist(get_prng_engine());
