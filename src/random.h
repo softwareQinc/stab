a29b90fad774542caf5038d62583a657fb650b3d
Vlad Gheorghiu <vsoftco@gmail.com>
2023-06-02 14:51:53 -0400

    fix random.h type deduction issue

--- src/random.h
+++ src/random.h
@@ -13,14 +13,26 @@ int random_bit(double p = 0.5);
 template <class T = int>
 T random_integer(T a = std::numeric_limits<T>::min(),
                  T b = std::numeric_limits<T>::max()) {
-    std::uniform_int_distribution<T> dist{a, b};
+    std::uniform_int_distribution<T> dist(a, b);
+    if (a > b) {
+        throw std::out_of_range("a > b");
+    }
+    if constexpr (!std::is_integral_v<T>) {
+        throw std::runtime_error("Type must be integral");
+    }
     return dist(get_prng_engine());
 }
 
 template <class T = double>
 T random_real(T a = std::numeric_limits<T>::min(),
               T b = std::numeric_limits<T>::max()) {
-    std::uniform_real_distribution<T> dist{a, b};
+    if (a >= b) {
+        throw std::out_of_range("a >= b");
+    }
+    if constexpr (!std::is_floating_point_v<T>) {
+        throw std::runtime_error("Type must be floating point");
+    }
+    std::uniform_real_distribution<T> dist(a, b);
     return dist(get_prng_engine());
 }
 } /* namespace stab */
